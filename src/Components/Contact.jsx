a0d635b8e655a544a445524a7b40a500e5585f98
Divyansh8x <Divyansh8x@users.noreply.github.com>
2024-04-19 01:30:26 +0530

    fixed!

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
             ></textarea>
 
             <button
-              type="submit" onClick={handleSubmit}
+              type="submit"
               className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
             >
               Let's talk
