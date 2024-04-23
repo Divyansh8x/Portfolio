c62ec1749da594f2e14d47ea80fbccb671786086
Divyansh8x <Divyansh8x@users.noreply.github.com>
2024-04-23 19:02:36 +0530

    fix Form Submit to my Email!

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
   const [formError, setFormError] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = () => {
-
-    if (!formData.name || !formData.email || !formData.message) {
-      setFormError("Please fill out all fields first!");
-      return;
-    }
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-    });
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setResult("");
     setFormError("");
+
+    const formData = new FormData(event.target);
+    formData.append("access_key", "c4decdf2-e6bb-4637-be2e-fc6de77cfa9f");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        setFormError(data.message);
+      }
+    } catch (error) {
+      console.error("Error submitting the form:", error);
+      setFormError("An error occurred while submitting the form.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -46,32 +52,21 @@ const Contact = () => {
         </div>
 
         <div className=" flex justify-center items-center">
-          <form
-            action="https://getform.io/f/qaloqwwb"
-            method="POST"
-            onSubmit={handleSubmit}
-            className="flex flex-col w-full md:w-1/2"
-          >
+          <form onSubmit={onSubmit} className="flex flex-col w-full md:w-1/2">
             <input
               type="text"
               name="name"
-              value={formData.name}
-              onChange={handleChange}
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 border-gray-500 rounded-md text-white focus:outline-none"
             />
             <input
               type="email"
               name="email"
-              value={formData.email}
-              onChange={handleChange}
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 border-gray-500 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
-              value={formData.message}
-              onChange={handleChange}
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md border-gray-500 text-white focus:outline-none"
@@ -80,11 +75,13 @@ const Contact = () => {
             <button
               type="submit"
               className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+              disabled={loading}
             >
-              Let's talk
+              {loading ? "Sending..." : "Let's talk"}
             </button>
 
             {formError && <p className="text-red-500">{formError}</p>}
+            {result && <p className="text-green-500">{result}</p>}
           </form>
         </div>
         <div>
